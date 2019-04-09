39196191840a9a1f711bd0830254a525b2fc53e6
STcraft <stefanvv@protonmail.com>
2019-04-09 19:47:51 +0200

    Config class overhaul for required values Still Need to add optional key loading..

--- Config.py
+++ Config.py
@@ -4,55 +4,93 @@ import json
 
 class Config:
 
-    _def_config = {
-        "mqtt_base_topic":  "irrigation",
-        "mqtt_server":      "192.168.1.30",
-        "mqtt_port":        1883,
-        "serial_port":      "/dev/ttyACM1",
-        "serial_baud":      9600,
-        "log_level":        5
+    # Required keys - Exit if not included in config file (Syntax = "key": data_type)
+    _required_keys = {
+        "mqtt_base_topic": str,
+        "mqtt_server": str,
+        "mqtt_port": int,
+        "serial_port": str,
+        "serial_baud": int,
+        "log_level": int
+    }
+
+    # Optional keys - Return default value if not included (Syntax = "key": [data_type, default_val})
+    _optional_keys = {
+        "mqtt_user": [str, None],
+        "mqtt_pass": [str, None]
     }
     
-    # This will be used after getting config data
-    _config = {}
+    _config = {}  # loaded configuration
     
-    _msg = SpecialMessages.SpecialMessages(_def_config["log_level"])
-    
-    def __init__(self, path):
-        try:
-            with open(path) as json_data:
-                d = json.load(json_data)
-                
-                self._override("mqtt_base_topic", d)
-                self._override("mqtt_server", d)
-                self._override("mqtt_port", d)
-                self._override("serial_port", d)
-                self._override("serial_baud", d)
-                self._override("log_level", d)
-                
-                json_data.close()
-                self._msg.i("Options overridden with config file.")
-        except FileNotFoundError:
-            self._msg.e("Config file not found! Using default config.")
-        except json.decoder.JSONDecodeError as e:
-            self._msg.e("Config syntax error: ")
-            self._msg.e(e)
-            exit(3)
-    
-    # TODO: Add Descriptions
-    def get(self, key, default = None):
+    _msg = SpecialMessages.SpecialMessages(5)
+
+    # Class initializer
+    # Can supply multiple fallback paths
+    def __init__(self, *path):
+        for file in path:
+            if self._load(file) == 0:
+                break
+        else:
+            self._msg.e("Quitting!")
+            exit(10)
+
+    # Get a configuration setting
+    def get(self, key, default=None):
         # Check if there has been a default value set
         if default is None:
             val = None
-            try:  # Try getting the values in loaded _config, otherwise try getting the default val in _def_config
-                val = self._config.get(key, self._def_config[key])
+            try:  # Try getting the values in loaded _config, otherwise return none
+                val = self._config.get(key, None)
             except KeyError as e:
                 self._msg.e("IN Config.py:  %s" % e)
-                val = self._def_config.get(key, None)
             return val
         else:   # Default val overridden by user
             return self._config.get(key, default)
     
-    def _override(self, key, json_obj):
-        self._config[key] = json_obj.get(key, self._def_config[key])
-    
+    # Try to load a config file by path, returns 0 on success
+    def _load(self, path):
+        try:
+            with open(path) as json_data:
+                d = json.load(json_data)
+
+                # Check config for any required keys and load values
+                if not self._check_config(d):
+                    return 4
+
+                # Close the file properly
+                json_data.close()
+
+                self._msg.i("Config file '%s' loaded." % path)
+                return 0
+
+        except FileNotFoundError:
+            self._msg.e("Config file '%s' not found!" % path)
+            return 1
+
+        except json.decoder.JSONDecodeError as e:
+            self._msg.e("Config syntax error: ")
+            self._msg.e(e)
+            return 2
+
+    # Check if the required keys are present, Returns boolean
+    def _check_config(self, json_obj):
+        # Iterate through all the required keys
+        for i in self._required_keys:
+            try:
+                # Check if the key is in the right format, otherwise return false
+                if type(json_obj[i]) is self._required_keys[i]:
+                    # Attempt to set get the key value, if the key doesn't exist, return false
+                    self._config[i] = json_obj[i]
+                else:
+                    self._msg.e("Required key '%s' is not an '%s' type!" % (i, self._required_keys[i]))
+                    break
+            except KeyError:
+                self._msg.e("Required key '%s' not found in config file!" % i)
+                break
+
+        # No errors found, Return true
+        else:
+            return True
+
+        # An error has happened, return false
+        return False
--- main.py
+++ main.py
@@ -4,63 +4,58 @@
         Also report back if the valve has been fully opened or fully closed (Switches inside the valve)
 """
 
-### IMPORTS
 import paho.mqtt.client as mqtt
 import json
-import serial as Serial
+import serial
 import asyncio
-import sys
 
-
-### LOCAL IMPORTS
 import GracefulKiller
 import SpecialMessages
 import Config
 
-### CONSTANTS
-POLLING_RATE = 3    # How many times per second it should check for serial events
+# CONSTANTS
+POLLING_RATE = 3  # How many times per second it should check for serial events
 
-### GLOBAL VARIABLES
+# GLOBAL VARIABLES
 should_exit_loop = False
 
-
-### CLASS DEFINITIONS
-if len(sys.argv[0]) >= 1:
-    print("USING test.json") 
-    config = Config.Config('test.json')
-else:
-    config = Config.Config('/data/options.json')
-
+# CLASS DEFINITIONS
+config = Config.Config('/data/options.json', 'test.json')
 client = mqtt.Client()
-serial = Serial.Serial()
+serial_port = serial.Serial()
 killer = GracefulKiller.GracefulKiller()
 msg = SpecialMessages.SpecialMessages(config.get("log_level"))
 
 
-### TOP FUNCTIONS
+"""
+    TOP FUNCTIONS
+"""
+
+
 # Program Main
 async def main():
     msg.s("Starting up!")
-    
+
     # Attempt connection to the serial device
     msg.n("Opening Serial port '%s'." % config.get("serial_port"))
-    serial.port = config.get("serial_port")
-    serial.baudrate = config.get("serial_baud")
+    serial_port.port = config.get("serial_port")
+    serial_port.baudrate = config.get("serial_baud")
     try:
-        serial.open()
+        serial_port.open()
     except Exception as e:
         msg.e(e)
         msg.e("Failed connecting to the serial port! Make sure you connected to the right serial port!")
         msg.n("Closing program...")
         # Exit program!
         exit(1)
-    
+
     # Attemt clearing the serial input buffer
-    serial.reset_input_buffer()
-    
+    serial_port.reset_input_buffer()
+
     # Attempt connection to the MQTT broker
     client.reinitialise(config.get("mqtt_base_topic"))
-    msg.n("Connecting MQTT client '%s' to %s:%s" % (config.get("mqtt_base_topic"), config.get("mqtt_server"), config.get("mqtt_port")))
+    msg.n("Connecting MQTT client '%s' to %s:%s" % (
+    config.get("mqtt_base_topic"), config.get("mqtt_server"), config.get("mqtt_port")))
     try:
         client.on_message = on_message
         client.connect(config.get("mqtt_server"), config.get("mqtt_port"))
@@ -70,60 +65,66 @@ async def main():
         msg.n("Closing program...")
         # Exit program!
         exit(2)
-    
+
     # Send a status notification
     msg.n("Publishing status message.")
     client.publish(config.get("mqtt_base_topic") + "/status", 1)
-    
+
     # Subscribe to enable recieving specific keys
     subscribe_topics()
-    
+
     # Loop - Don't worry, A SIGINT or SIGTERM will safely exit any loops (Hopefully! ;) )
     msg.s("Ready!")
     client.loop_start()
     await monitor_serial_input()
-    
-    
+
+
 # Program Stop
 def stop():
     msg.s("Shutting down!")
     should_exit_loop = True
     client.loop_stop()
-    
+
     msg.n("Publishing status message.")
     client.publish(config.get("mqtt_base_topic") + "/status", 0)
-    
+
     msg.n("Disconnecting.")
     client.disconnect()
-    
+
     # Making sure the Serial I/O buffer is cleared
-    serial.flush()
-    serial.reset_input_buffer()
-    serial.close()
-    
+    serial_port.flush()
+    serial_port.reset_input_buffer()
+    serial_port.close()
+
     msg.s("Program ended.")
 
-# Monitor Serial input, parse it and then publish the values to MQTT TODO: WHILE LOOPS ARE VERY CPU INTENSIVE!!
+
+# Monitor Serial input, parse it and then publish the values to MQTT
 async def monitor_serial_input():
     while not killer.kill_now:
-        if serial.inWaiting() > 0:
-            # Read Serial input
-            _data = serial.readline().decode("utf-8").rstrip()
-            
-            msg.d("UART -> This    " + _data)
-            
-            # Parse JSON and publish sensor data via MQTT
-            try:
-                _j = json.loads(_data)
-            
-                msg.d("This -> MQTT    Publishing data...")
-                publish_data(_j)                    
-                msg.d("This -> MQTT    Publishing done!")
-                
-            except ValueError as e:
-                msg.e("monitor_serial_input():  %s" % e)
-                
+        try:
+            if serial_port.inWaiting() > 0:
+                # Read Serial input
+                _data = serial_port.readline().decode("utf-8").rstrip()
+
+                msg.d("UART -> This    " + _data)
+
+                # Parse JSON and publish sensor data via MQTT
+                try:
+                    _j = json.loads(_data)
+
+                    msg.d("This -> MQTT    Publishing data...")
+                    publish_data(_j)
+                    msg.d("This -> MQTT    Publishing done!")
+
+                except ValueError as e:
+                    msg.e("monitor_serial_input():  %s" % e)
+
+        except UnicodeDecodeError as e:
+            msg.e(e)
+
         await asyncio.sleep(1 / POLLING_RATE)
+
 
 # Subscribe to topics
 def subscribe_topics():
@@ -132,9 +133,11 @@ def subscribe_topics():
     client.subscribe(config.get("mqtt_base_topic") + "/targetValvePos")
     client.subscribe(config.get("mqtt_base_topic") + "/reportInterval")
 
+
 # Publish sensor data to MQTT
 def publish_data(json_obj):
-    # Get json data and publish it - If a certain key wasn't recieved, it will not publish any data with that key (avoids sudden null payloads)
+    # Get json data and publish it - If a certain key wasn't received,
+    # it will not publish any data with that key (avoids sudden null payloads)
     attempt_publish_data(json_obj, "timeStamp")
     attempt_publish_data(json_obj, "airHumidity")
     attempt_publish_data(json_obj, "flowRate")
@@ -142,22 +145,23 @@ def publish_data(json_obj):
     attempt_publish_data(json_obj, "enclosureTemperature")
     attempt_publish_data(json_obj, "movementDetected")
     attempt_publish_data(json_obj, "currentValvePos")
-    attempt_publish_data(json_obj, "soilHumidity", jkey = "avgSoilHumidity")
-    
+    attempt_publish_data(json_obj, "soilHumidity", jkey="avgSoilHumidity")
+
     attempt_publish_data(json_obj, "targetValvePos", "/get")
     attempt_publish_data(json_obj, "mode", "/get")
     attempt_publish_data(json_obj, "flowLimit", "/get")
-    
+
     # soilHumidity Will need to be split up into sub values
-    try: 
+    try:
         for i in range(0, len(json_obj["soilHumidity"])):
             client.publish(config.get("mqtt_base_topic") + "/soilHumidity/%s" % i, json_obj["soilHumidity"][i])
     except KeyError as e:
         msg.d("attempt_publish_data():  %s" % e)
         pass
 
+
 # If the provided key exists, get the payload and publish it, otherwise ignore it and do not publish
-def attempt_publish_data(json_obj, key, topic_suffix = "", jkey = None):
+def attempt_publish_data(json_obj, key, topic_suffix="", jkey=None):
     try:
         if jkey is None:
             client.publish("%s/%s%s" % (config.get("mqtt_base_topic"), key, topic_suffix), json_obj[key])
@@ -167,17 +171,18 @@ def attempt_publish_data(json_obj, key, topic_suffix = "", jkey = None):
         msg.d("attempt_publish_data():  %s" % e)
         pass
 
-# Perform this function when a MQTT message has been recieved
+
+# Perform this function when a MQTT message has been received
 def on_message(client, userdata, message):
     try:
         # Get and decode payload message
         payload = str(message.payload.decode("utf-8"))
-        
+
         msg.d("MQTT -> This    Topic: %s    Payload: %s" % (message.topic, payload))
-        
+
         # Get which message should be relayed (Note: Values below 0 will supposedly get ignored)
         output_msg = {"mode": -1, "flowLimit": -1, "targetValvePos": -1, "reportInterval": -1}
-        
+
         if message.topic == config.get("mqtt_base_topic") + "/mode":
             output_msg["mode"] = str2bool(payload)
         elif message.topic == config.get("mqtt_base_topic") + "/flowLimit":
@@ -188,22 +193,23 @@ def on_message(client, userdata, message):
             output_msg["reportInterval"] = int(payload)
         else:
             msg.e("Unhandled Topic: %s" % message.topic)
-        
+
         # Relay any matched message to Serial and make sure it's written properly
         output_json = json.dumps(output_msg)
         msg.d("This -> UART    %s" % str(output_json))
-        serial.write(str(str(output_json) + "\n").encode('utf-8'))
-        serial.flush()
-        
+        serial_port.write(str(str(output_json) + "\n").encode('utf-8'))
+        serial_port.flush()
+
     except Exception as e:
         print("on_message():  %s" % e)
+
 
 # Convert string to boolean - Supports a variety of values for ease of use
 def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1", "on", "enable")
 
 
-### START THE MAIN LOOP - DON'T TOUCH
+# START THE MAIN LOOP - DON'T TOUCH
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
